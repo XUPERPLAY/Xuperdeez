a6f0a2eeea08972942161023457b5ae38ce0caa1
XUPERPLAY <tintxicasa2@gmail.com>
2025-08-26 17:40:44 +0200

    Delete server directory

--- server/routes/music.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const musicController = require('../controllers/musicController');
-
-router.get('/songs', musicController.getSongs);
-router.post('/songs', musicController.uploadSong);
-
-module.exports = router;
